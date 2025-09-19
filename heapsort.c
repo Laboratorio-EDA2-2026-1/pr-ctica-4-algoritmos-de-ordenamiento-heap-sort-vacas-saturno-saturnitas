10cc65fc49327172afe059e1a6cc52f8d4d97bb3
Aaron <carranzaalexisaaron245@gmail.com>
2025-09-19 08:46:08 -0600

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -19,9 +19,14 @@ void heap_sort(int a[], int n);
 
 /* MAIN de prueba mínima */
 int main(void) {
-    int n; if (scanf("%d", &n) != 1 || n <= 0) return 0;
+    int n;
+
+    if (scanf("%d", &n) != 1 || n <= 0) return 0;
+
     int *a = (int*)malloc(sizeof(int) * n);
+
     if (!a) return 0;
+
     for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
 
     heap_sort(a, n);
@@ -30,7 +35,7 @@ int main(void) {
         if (i) putchar(' ');
         printf("%d", a[i]);
     }
-    putchar('\n');
+    putchar(' ');
 
     free(a);
     return 0;
@@ -38,40 +43,44 @@ int main(void) {
 
 /* IMPLEMENTAR AQUÍ */
 void max_heapify(int a[], int n, int i) {
-    int l = 2 * i + 1;  // hijo izquierdo
-    int r = 2 * i + 2;  // hijo derecho
-    int largest = i;
+    int l=(2*i)+1;
+    int r=2*i+2;
+    int largo=i;
 
-    if (l < n && a[l] > a[largest])
-        largest = l;
-    if (r < n && a[r] > a[largest])
-        largest = r;
+    
 
-    if (largest != i) {
-        int tmp = a[i];
-        a[i] = a[largest];
-        a[largest] = tmp;
-        max_heapify(a, n, largest);
+    if(l<n && a[l]>a[largo]){
+        largo=l;
     }
+
+    if (r<n && a[largo]<a[r])
+    {
+        largo=r;
+    }
+
+    if (largo!=i)
+    {
+        int temporal=a[i];
+        a[i]=a[largo];
+        a[largo]=temporal;
+        max_heapify(a,n,largo);
+    }
+    
+    
 }
 
 void build_max_heap(int a[], int n) {
-    // El último nodo que no es hoja está en n/2 - 1
-    for (int i = n / 2 - 1; i >= 0; --i) {
-        max_heapify(a, n, i);
+    for (int i=n/2-1; i>=0;--i){
+        max_heapify(a,n,i);
     }
 }
 
 void heap_sort(int a[], int n) {
-    build_max_heap(a, n);
-
-    for (int i = n - 1; i > 0; --i) {
-        // Intercambiar la raíz (máximo) con el último
-        int tmp = a[0];
-        a[0] = a[i];
-        a[i] = tmp;
-
-        // Reducir tamaño del heap y restaurar la propiedad
-        max_heapify(a, i, 0);
+    build_max_heap(a,n);
+    for(int i=n-1; i>0;--i){
+        int temporal=a[0];
+        a[0]=a[i];
+        a[i]=temporal;
+        max_heapify(a,i,0);
     }
 }

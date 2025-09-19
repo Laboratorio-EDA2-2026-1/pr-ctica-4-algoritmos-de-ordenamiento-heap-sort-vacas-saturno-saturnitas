ecde29eb25db802da9c0002858caf5e9e497193c
victor_ostria <victorostriamacario@gmail.com>
2025-09-19 08:41:44 -0600

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -39,13 +39,40 @@ int main(void) {
 
 /* IMPLEMENTAR AQUÍ */
 void max_heapify(int a[], int n, int i) {
-    /* TODO */
+    int l = 2 * i + 1;  // hijo izquierdo
+    int r = 2 * i + 2;  // hijo derecho
+    int largest = i;
+
+    if (l < n && a[l] > a[largest])
+        largest = l;
+    if (r < n && a[r] > a[largest])
+        largest = r;
+
+    if (largest != i) {
+        int tmp = a[i];
+        a[i] = a[largest];
+        a[largest] = tmp;
+        max_heapify(a, n, largest);
+    }
 }
 
 void build_max_heap(int a[], int n) {
-    /* TODO */
+    // El último nodo que no es hoja está en n/2 - 1
+    for (int i = n / 2 - 1; i >= 0; --i) {
+        max_heapify(a, n, i);
+    }
 }
 
 void heap_sort(int a[], int n) {
-    /* TODO */
+    build_max_heap(a, n);
+
+    for (int i = n - 1; i > 0; --i) {
+        // Intercambiar la raíz (máximo) con el último
+        int tmp = a[0];
+        a[0] = a[i];
+        a[i] = tmp;
+
+        // Reducir tamaño del heap y restaurar la propiedad
+        max_heapify(a, i, 0);
+    }
 }
